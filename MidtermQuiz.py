96e6a9fefd5058e16951f0804f53920f4214d5a1
JFMilan06 <123443594+JFMilan06@users.noreply.github.com>
2023-04-04 15:24:40 +0800

    Add files via upload

---
+++ MidtermQuiz.py
@@ -0,0 +1,26 @@
+class TempConversion():
+  def __init__(self, temp):
+    self.temp = temp
+
+  def FtoC(self):
+    return (self.temp-32)*5/9
+  def KtoC(self):
+    return self.temp-273.15
+  def CtoF(self):
+    return (self.temp*9/5)+32
+  def KtoF(self):
+    return ((1.8*self.temp)-459.67)
+  def CtoK(self):
+    return self.temp+273.15
+  def FtoK(self):
+    return ((self.temp+459.67)/1.8)
+
+
+temp = int(input("input temperature:"))
+tempconversion = TempConversion(temp)
+print(f"The value of the temperature from Fahrenheit to Celsius is {tempconversion.FtoC()}")
+print(f"The value of the temperature from Kelvin to Celsius is {tempconversion.KtoC()}")
+print(f"The value of the temperature from Celsius to Fahrenheit is {tempconversion.CtoF()}")
+print(f"The value of the temperature from Kelvin to Fahrenheit is {tempconversion.KtoF()}")
+print(f"The value of the temperature from Celsius to Kelvin is {tempconversion.CtoK()}")
+print(f"The value of the temperature from Fahrenheit to Kelvin is {tempconversion.FtoK()}")
